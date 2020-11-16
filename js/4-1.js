58a89fd8dca8fd5af8543e2515dfe526b63edd60
Dmytro <mityaua@gmail.com>
2020-11-16 18:04:45 +0200

    Добавляет решение 1й задачи и условие для 2й

--- js/4-1.js
+++ js/4-1.js
@@ -1,12 +1,18 @@
 // Write code under this line
-const addIndex = (element, index) => element;
+// 1. По условиям задачи возвращаем через первую стрелочную функцию сумму параметров;
+// 2. Также для второй функции делаем вычитание для параметров;
+// 3. При создании нового массива numbers для new Array используем длинну массива https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/Array ;
+
+
+const addIndex = (element, index) => element + index;
+
 // Write code under this line
-const subIndex = (element, index) => element;
+const subIndex = (element, index) => element - index;
 
 function mapArray(array, cb) {
   'use strict';
   // Write code under this line
-  const numbers = new Array();
+  const numbers = new Array(array.length);
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
     const index = i;
@@ -17,8 +23,8 @@ function mapArray(array, cb) {
 
 const arr = [1, 2, 3, 4, 5];
 
-//console.log(mapArray(arr, addIndex));
+console.log(mapArray(arr, addIndex));
 // [1, 3, 5, 7, 9]
 
-//console.log(mapArray(arr, subIndex));
+console.log(mapArray(arr, subIndex));
 // [1, 1, 1, 1, 1]
