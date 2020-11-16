279cd0ac06a7687067500a5b646c23a071e51a4b
Dmytro <mityaua@gmail.com>
2020-11-16 22:06:31 +0200

    Добавляет решени 2й задачи и условие 3й

--- js/4-2.js
+++ js/4-2.js
@@ -1,3 +1,7 @@
+// 1. Внутри функции filterArray создаем условие через if;
+// 2. Условие - если коллбек функция (в которую передано элемент массива, индекс элемента массива, и сам массив) возращает true;
+// 3. Тогда добавляем элемент в новый массив numbers;
+
 const isUniq = (element, index, arr) => arr.indexOf(element) === index;
 const isEven = element => element % 2 === 0;
 
@@ -8,6 +12,9 @@ function filterArray(array, cb) {
     const element = array[i];
     const index = i;
     // Write code under this line
+    if (cb(element, index, array)) {
+      numbers.push(element);
+    }
   }
   return numbers;
 }
